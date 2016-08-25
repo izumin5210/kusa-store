5644210e0d55f1706f936bf8d8829da0062a1f35
izumin5210 <masayuki@izumin.info>
2016-08-26 02:20:18 +0900

    :shirt: Remove linter warnings and errors

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const AWS     = require('aws-sdk');
-const axios   = require('axios');
-const cheerio = require('cheerio');
-const moment  = require('moment');
+const AWS     = require("aws-sdk");
+const axios   = require("axios");
+const cheerio = require("cheerio");
+const moment  = require("moment");
 
-AWS.config.region = 'ap-northeast-1';
-const bucketName = 'kusa-store';
+AWS.config.region = "ap-northeast-1";
+const bucketName = "kusa-store";
 const s3bucket = new AWS.S3({ params: { Bucket: bucketName }});
 
-const graphSelector = '.js-calendar-graph-svg rect'
+const graphSelector = ".js-calendar-graph-svg rect";
 
 exports.handler = (event, context) => {
-  axios.get('https://github.com/izumin5210')
+  axios.get("https://github.com/izumin5210")
     .then(res => {
-      const key = `${moment().format('YYYYMMDDThhmmssSSS')}.json`;
+      const key = `${moment().format("YYYYMMDDThhmmssSSS")}.json`;
       const $ = cheerio.load(res.data);
-      const body = $(graphSelector).map((_i, e) => { date: $(e).data('date'), count: $(e).data('count') }).get();
+      const body = $(graphSelector).map((_i, e) => ({ date: $(e).data("date"), count: $(e).data("count") })).get();
       return s3bucket.putObject({ Key: key, Body: body });
     })
-    .then(data => {
-      context.succeed('Successfully uploaded data to myBucket/myKey');
+    .then(() => {
+      context.succeed("Successfully uploaded data to myBucket/myKey");
     })
     .catch(err => {
       context.fail(`Error fetching data: ${err}`);
