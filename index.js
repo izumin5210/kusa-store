92685ce13182ae549eef2a5d9a9e46fdf99dbcfb
izumin5210 <masayuki@izumin.info>
2016-08-26 04:42:59 +0900

    :art: Split the handler function

--- index.js
+++ index.js
@@ -9,16 +9,27 @@ const s3bucket = new AWS.S3({ params: { Bucket: bucketName } });
 
 const graphSelector = ".js-calendar-graph-svg rect";
 
-exports.handler = (event, context) => {
-  axios.get("https://github.com/izumin5210")
+const githubUsername = "izumin5210";
+
+const uploadToS3 = body => {
+  const key = `${moment().format("YYYYMMDDThhmmssSSS")}.json`;
+  return s3bucket.upload({ Key: key, Body: body }).promise();
+};
+
+const fetchKusa = username => {
+  return axios.get(`https://github.com/${username}`)
     .then(res => {
-      const key = `${moment().format("YYYYMMDDThhmmssSSS")}.json`;
       const $ = cheerio.load(res.data);
       const body = $(graphSelector).map((_i, e) => {
         return { date: $(e).data("date"), count: $(e).data("count") };
       }).get();
-      return s3bucket.upload({ Key: key, Body: body }).promise();
-    })
+      return Promise.resove(body);
+    });
+};
+
+exports.handler = (event, context) => {
+  fetchKusa(githubUsername)
+    .then(uploadToS3)
     .then(() => {
       context.succeed("Successfully uploaded data to myBucket/myKey");
     })
